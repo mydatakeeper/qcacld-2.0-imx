b6e8f02ad0144396ff52a8e1ccbf464923277c0d
Suresh Kumar Gautam <sgkuma@codeaurora.org>
2017-07-13 18:29:27 +0530

    qcacld-2.0: Define ini item gEnableNoAck
    Define new ini item to configure “No ACK”
    functionality.
    
    Change-Id: Ibcbece095af2a12bbe17777062d9b4ef7aca235a
    CRs-fixed: 2077588

--- CORE/VOSS/inc/vos_api.h
+++ CORE/VOSS/inc/vos_api.h
@@ -339,6 +339,8 @@ v_VOID_t vos_fwDumpReq(tANI_U32 cmd, tANI_U32 arg1, tANI_U32 arg2,
 
 v_BOOL_t vos_is_packet_log_enabled(void);
 
+v_BOOL_t vos_config_is_no_ack(void);
+
 v_U64_t vos_get_monotonic_boottime(void);
 
 /**
